ff66509e5afa0cb9eb2d5b956dd7c314a74eccb7
markmadden <mark.madden@spsmail.cuny.edu>
2014-09-14 18:48:42 -0400

    for jenkins to look at task1

---
+++ task_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' By Mark Madden for lesson 03 homework '''
+
+QUESTION_01 = raw_input('Is today a sunny day? ')
+
+QUESTION_02 = int(raw_input('What is your favorite number? '))
