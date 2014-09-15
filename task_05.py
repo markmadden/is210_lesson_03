dc8361025091c5830a15be1c0065713a9a1b89c3
markmadden <mark.madden@spsmail.cuny.edu>
2014-09-15 00:24:47 -0400

    task5

---
+++ task_05.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' task5 mark madden, incomplete at this time '''
+
+name = raw_input('What is your name? ')
+principal = int(raw_input('What is the amountf your principle (the amount being
+                          borrowed)? ))
+years = int(raw_input('For how many years is this loan being borrowed? '))
