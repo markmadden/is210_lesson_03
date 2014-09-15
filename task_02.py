f55947120b845ccd6890a10a7adfb4a8edc4ce91
markmadden <mark.madden@spsmail.cuny.edu>
2014-09-14 20:14:15 -0400

    task2try3

--- task_02.py
+++ task_02.py
@@ -2,17 +2,15 @@
 # -*- coding: utf-8 -*-
 ''' by mark madden for week3 homework '''
 
-BP_STATUS = int(raw_input('Type your numeric systolic blood pressure please: '))
-if BP_STATUS < 90:
-    print "Your status is currently: Low."
-elif BP_STATUS < 120 and BP_STATUS > 89:
-    print "Your status is currently: Ideal."
-elif BP_STATUS < 140 and BP_STATUS > 119:
-    print "Your status is currently: Warning!"
-elif BP_STATUS < 160 and BP_STATUS > 139:
-    print "Your status is currently: High."
-elif BP_STATUS > 159:
-    print "Your status is currently: Emergency!"
-else:
-    print "You entered an invalid input."
-    
+SYSTOLIC = int(raw_input('Type your numeric systolic blood pressure please: '))
+if SYSTOLIC < 90:
+    BP_STATUS = "Low."
+elif SYSTOLIC < 120 and SYSTOLIC > 89:
+    BP_STATUS = "Ideal."
+elif SYSTOLIC < 140 and SYSTOLIC > 119:
+    BP_STATUS = "Warning!"
+elif SYSTOLIC < 160 and SYSTOLIC > 139:
+    BP_STATUS = "High."
+elif SYSTOLIC > 159:
+    BP_STATUS = "Emergency."
+print "Your status is currently: {0}".format(BP_STATUS)
